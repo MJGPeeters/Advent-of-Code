d3739f8d823df04c0f99ef98942aaf7bc6d8e52a
MJGPeeters <mjgpeeters@gmail.com>
2024-12-14 22:37:26 +0100

    Tried solving 2023 - Day 1 - Part II

--- 2023 - Python/AoC_2023_1.py
+++ 2023 - Python/AoC_2023_1.py
@@ -1,4 +1,32 @@
-with open("Tests/Test_2023_1.txt", "r") as f:
-    file_content = f.read()
+with open("Tests/Test_2023_1.txt", "r") as file:
+    fileContent = file.read()
 
-print(file_content)
+fileLines = fileContent.splitlines()
+
+tmp1 = []
+tmp2 = []
+
+sum1 = 0
+sum2 = 0
+
+numLongList  = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+numShortList = ['1','2','3','4','5','6','7','8','9']
+
+for fileLine in fileLines:
+    tmp1 = [character for character in fileLine if character.isdigit()]
+    sum1 += int(tmp1[0] + tmp1[-1])
+
+    tmp2 = [character for character in fileLine if character in numShortList or character in numLongList]
+
+    # for index, dummy in enumerate(fileLine):
+    #     tmp2 = [numShortList[index] for numLongList[index] in fileLine]
+    
+    # print(tmp2)
+
+
+
+
+
+
+print(sum1)
+print(sum2)
