0eae5fed8631f78c577233a9d0218a52b3a304e9
MJGPeeters <mjgpeeters@gmail.com>
2024-12-18 17:38:37 +0100

    2024 - Day 17 - Solved Part I

---
+++ 2024 - Matlab/AoC_2024_17.m
@@ -0,0 +1,123 @@
+%% Preamble
+adventDay = 17;
+testBool = 0;
+
+if testBool
+    fileName = "Tests/Test_2024_" + adventDay + ".txt";
+else
+    fileName = "Inputs/Input_2024_" + adventDay + ".txt"; 
+end
+
+%% Read data
+fileID = fopen(fileName);
+
+line = fgetl(fileID);
+program = [];
+
+while ~isscalar(line)
+    tmp = strsplit(line,':');
+    if ~isempty(tmp{1,1})
+        if tmp{1,1}(1)=='P'
+            programCell = strsplit(line,':');
+            programCell = strsplit(programCell{1,2},',');
+            for i=1:numel(programCell)
+                program = [program double(string(programCell{i}))];
+            end
+        elseif all(tmp{1,1}=='Register A')
+            regA = double(string(tmp{1,2}));
+        elseif all(tmp{1,1}=='Register B')
+            regB = double(string(tmp{1,2}));
+        elseif all(tmp{1,1}=='Register C')
+            regC = double(string(tmp{1,2}));
+        end
+    end
+    line = fgetl(fileID);
+    i = i+1;
+end
+fclose(fileID);
+
+%% Solve part I
+tic
+
+programLength = numel(program);
+pointer = 0;
+output = [];
+
+while pointer<programLength-1
+    opcode = program(pointer+1);
+    operand = program(pointer+2);
+    if regA==540861 && regB==618126
+        disp('test')
+    end
+    [pointer,regA,regB,regC,output] = adventComputer(pointer,opcode,operand,regA,regB,regC,output);
+    disp([regA,regB, regC])
+end
+
+outputString = '';
+for i=1:numel(output)
+    outputString = [outputString char(string(output(i))) ','];
+end
+
+time1 = toc;
+
+%% Display results of part I
+out1 = sprintf('The output of the program is %s.', output);
+tim1 = sprintf('Calculation took %f seconds.', time1);
+disp(out1)
+disp(tim1)
+
+% %% Solve part II
+% tic 
+% 
+% % in Astar function, do something when tentativeGScore equals the gScore of
+% % the neighbor. That means that a path is found with the same score, so
+% % another best path. Maybe just change < to =, and save a list of all the
+% % visited squares instead of a dict (the order is not important now). Maybe
+% % just keep a map-like array with all zeros, and mark with ones the squares
+% % that are visited. At the end, sum all the ones. Or even simply keep a
+% % counter of the number of visited squares. Then double-counting is a risk.
+% 
+% pathArray = AstarAllPaths(start,goal,mapArray);
+% 
+% solutionMap = ones(size(mapArray));
+% solutionMap(mapArray==1) = 0;
+% 
+% imagesc(mapArray + 0.5*pathArray)
+% colormap gray
+% 
+% % score = -1;
+% % pos = 0;
+% % 
+% % test = numel(path);
+% % 
+% % for i=1:numel(path)
+% %     posPrev = pos;
+% %     pos = (path(i) - mod(path(i),10))/10;
+% % 
+% %     if pos==posPrev
+% %         score = score+1000;
+% %     else
+% %         score = score+1;
+% %     end
+% % 
+% %     % tmp = path(i);
+% %     % pathArray(i,3) = mod(tmp,10);
+% %     % 
+% %     % tmp = (tmp - mod(tmp,10))/10;
+% %     % pathArray(i,1) = mod(tmp,1000);
+% %     % pathArray(i,2) = (tmp - mod(tmp,1000))/1000;
+% %     % 
+% %     % solutionMap(pathArray(i,1),pathArray(i,2)) = 0.7;
+% % end
+% 
+% result1 = score;
+% 
+% result2 = 0;
+% 
+% time2 = toc;
+% 
+% %% Display results of part II
+% out2 = sprintf('%d tiles are part of at least one of the best paths.', result2);
+% tim2 = sprintf('Calculation took %f seconds.', time2);
+% disp(out2)
+% disp(tim2)
