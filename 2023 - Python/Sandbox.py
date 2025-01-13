e5775721803dec6135472506f61015408de2bdb4
MJGPeeters <mjgpeeters@gmail.com>
2025-01-13 23:31:12 +0100

    2023 - Added Sandbox.py

--- 2023 - Python/Sandbox.py
+++ 2023 - Python/Sandbox.py
@@ -1,12 +1,18 @@
-import numpy as np
+import math as m, re
 
-x = [1,2,3,4,5]
+testName = 'Tests/Test_2023_3.txt'
+inputName = 'Inputs/Input_2023_3.txt'
 
-y = np.max(x)
+board = list(open(testName))
+chars = {(r, c): [] for r in range(10) for c in range(10) if board[r][c] not in '01234566789.'}
 
-a = "two1nine"
+for r, row in enumerate(board):
+    for n in re.finditer(r'\d+', row):
+        edge = {(r, c) for r in (r-1, r, r+1)
+                       for c in range(n.start()-1, n.end()+1)}
 
-b = a.find("two")
-c = a.find("nine")
+        for o in edge & chars.keys():
+            chars[o].append(int(n.group()))
 
-x
+print(sum(sum(p)    for p in chars.values()),
+      sum(m.prod(p) for p in chars.values() if len(p)==2))
