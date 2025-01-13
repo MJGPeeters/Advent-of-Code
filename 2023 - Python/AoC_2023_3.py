42bb4fc20ca8e4b310814b4a65f6c17a16b1d818
MJGPeeters <mjgpeeters@gmail.com>
2025-01-13 19:40:05 +0100

    2023 - Day 3 - Solved part II

--- 2023 - Python/AoC_2023_3.py
+++ 2023 - Python/AoC_2023_3.py
@@ -16,7 +16,12 @@ array = ['.' + line + '.' for line in fileLines]
 array.insert(0, '.'*len(array[0]))
 array.insert(len(array), '.'*len(array[0]))
 
+# Initialize array with zeros for part II
+numIDarray = [[0 for col in row] for row in array]
+
 sum1 = 0
+numID = 1
+d = dict()
 
 for lineNumber, line in enumerate(array):
     numbers = {el for el in re.split('[^0-9]', line) if el.isdigit()}
@@ -34,16 +39,33 @@ for lineNumber, line in enumerate(array):
                 if numCheck:
                     sum1 += int(number)
 
+                numIDarray[lineNumber][idxStart:idxEnd] = [numID]*(idxEnd - idxStart)
+                d[numID] = number
+                numID += 1
+
 endTime1 = timer()
 
 print(sum1)
 print('Time elapsed: {:.6f} s'.format(endTime1 - startTime1))
 
-# # Part II
+# Part II
 
-# startTime2 = timer()
+startTime2 = timer()
 
-# endTime2 = timer()
+sum2 = 0
 
-# print(sum2)
-# print('Time elapsed: {:.6f} s'.format(endTime2 - startTime2))
+for lineNumber, line in enumerate(array):
+    idxs = [m.start() for m in re.finditer('\*', line)]
+
+    for idx in idxs:
+        numIDs = {i for row in numIDarray[lineNumber-1:lineNumber+2] for i in row[idx-1:idx+2]}
+
+        sortedIDs = sorted(numIDs)
+
+        if len(sortedIDs)==3:
+            sum2 += int(d.get(sortedIDs[1]))*int(d.get(sortedIDs[2]))
+
+endTime2 = timer()
+
+print(sum2)
+print('Time elapsed: {:.6f} s'.format(endTime2 - startTime2))
