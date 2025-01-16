16b0fd6def52f55c98fe55e7d8276fdb39b7d3c7
MJGPeeters <mjgpeeters@gmail.com>
2025-01-16 12:34:43 +0100

    2023 - Day 7 - Solved part II

--- 2023 - Python/AoC_2023_7.py
+++ 2023 - Python/AoC_2023_7.py
@@ -59,14 +59,78 @@ endTime1 = timer()
 print(winnings)
 print('Time elapsed: {:.6f} s'.format(endTime1 - startTime1))
 
-# # Part II
-# startTime2 = timer()
+# Part II
+startTime2 = timer()
 
-# sum2 = 0
+sum2 = 0
 
+# Every hand with at least one J in it has to be checked
+FiOAK, FoOAK, FH, ThOAK, TP, OP, HC = [], [], [], [], [], [], []
 
+for line in fileLines:
+    hand, bid = tuple(line.split())
 
-# endTime2 = timer()
+    counts = dict()
+    for card in hand:
+        counts[card] = counts.get(card, 0) + 1
 
-# print(sum2)
-# print('Time elapsed: {:.6f} s'.format(endTime2 - startTime2))
+    counts = sorted([*counts.values()], reverse=True)
+    counts = counts[0:2]
+
+    origCards = 'AKQJT'
+    sortCards = 'FED0B'
+    mytable = str.maketrans(origCards, sortCards)
+    newHand = hand.translate(mytable)
+
+    comb = [newHand, bid]
+    numJacks = newHand.count('0')
+
+    if counts[0]==5:
+        FiOAK.append(comb)
+    elif counts[0:2]==[4,1]:
+        if numJacks==1 or numJacks==4:
+            FiOAK.append(comb)
+        else:
+            FoOAK.append(comb)
+    elif counts[0:2]==[3,2]:
+        if numJacks==2 or numJacks==3:
+            FiOAK.append(comb)
+        else:
+            FH.append(comb)
+    elif counts[0:2]==[3,1]:
+        if numJacks==1 or numJacks==3:
+            FoOAK.append(comb)
+        else:
+            ThOAK.append(comb)
+    elif counts[0:2]==[2,2]:
+        if numJacks==1:
+            FH.append(comb)
+        elif numJacks==2:
+            FoOAK.append(comb)
+        else:
+            TP.append(comb)
+    elif counts[0:2]==[2,1]:
+        if numJacks==1 or numJacks==2:
+            ThOAK.append(comb)
+        else:
+            OP.append(comb)
+    else:
+        if numJacks==1:
+            OP.append(comb)
+        else:
+            HC.append(comb)
+
+handTypes = [HC, OP, TP, ThOAK, FH, FoOAK, FiOAK]
+
+baseRank = 1
+winnings = 0
+
+for handType in handTypes:
+    sortedHands = sorted(handType)
+    winnings += sum([(rank + baseRank)*int(bid) for rank, (hand, bid) in enumerate(sortedHands)])
+    baseRank += len(handType)
+
+endTime2 = timer()
+
+print(winnings)
+print('Time elapsed: {:.6f} s'.format(endTime2 - startTime2))
