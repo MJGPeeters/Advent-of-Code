812d4958607a19ce0f06ac48e818bc72f26913e0
MJGPeeters <mjgpeeters@gmail.com>
2025-01-18 18:57:54 +0100

    2023 - Day 12 - Attempts at part I

--- 2023 - Python/AoC_2023_12.py
+++ 2023 - Python/AoC_2023_12.py
@@ -2,58 +2,115 @@ from timeit import default_timer as timer
 
 startTime1 = timer()
 
+def replace_index(string, index, replacement):
+    """
+    Replace text in string starting at given index
+ 
+    Inputs:
+    string - String
+    index - Integer
+    replacement - String
+
+    Outputs:
+    string
+    """
+    return f'{string[:index]}{replacement}{string[index+len(replacement):]}'
+
+
 def count_arrangements(springs, groups):
     """
     Count possible arrangements for a given arrangement of springs and groups
 
     Inputs:
-    springs - List of groups of '?#' ['??##', '#??#', ...]
+    springs - List of groups of '?' and '#' ['??##', '#??#', ...]
     groups - List of integers [2, 3, ...]
 
     Outputs:
     Integer - Number of possible arrangements
     """
 
+    numArrangements = 0
+
+    fills = ['#'*n for n in groups]
+
+    lenFills = [len(fill) for fill in fills]
+
+    tmpSprings = springs[:]
+
+    tmpSpringNum = 0
+    tmpIndex = 0
+
+    for fill, lenFill in zip(fills, lenFills):
+        tmpSprings[tmpSpringNum] = replace_index(tmpSprings[tmpSpringNum], tmpIndex, fill)
+        tmpIndex += lenFills
+
+
+    if sum([spring.count('#') for spring in tmpSprings])==sum(groups):
+        numArrangements += 1
     
+    return numArrangements
+
+
+
+
+
+
+
+    # Place all groups in the first available position
+    # Check if it fits
+
+    # Place all but last group in the first available positions
+    # Move last group from first to last available position, check if it all works
+
+    # Place all but last two groups in the first available positions
+    # Move second to last group one space
+
+    # Move recursively this way, until the first instance of a level does not work anymore
+    # For example: group 1 in first position, group 2 moved one right, group 3 and 4 in first positions
+
+    # Keep track of possible arrangements
+
+
+
+
+
 
 testName = 'Tests/Test_2023_12.txt'
 inputName = 'Inputs/Input_2023_12.txt'
 
-with open(inputName, "r") as file:
+with open(testName, "r") as file:
     fileLines = file.read().splitlines()
+
+ans1 = 0
 
 for line in fileLines:
     springs, groups = line.split()
 
     springs = list(filter(None, springs.split('.')))
-    groups = groups.split(',')
+    groups = [n for n in map(int, groups.split(','))]
 
-    while len(springs[0]) < int(groups[0]):
+    # Check if all damaged springs are already accounted for
+    if sum([spring.count('#') for spring in springs])==sum(groups):
+        ans1 += 1
+        continue
+
+    # Remove impossible-to-fill or already filled parts of springs (and of groups for already filled)
+    while len(springs[0])<=groups[0]:
         del springs[0]
-    
-    while len(springs[-1]) < int(groups[-1]):
+        if len(springs[0])==groups[0]:
+            del groups[0]
+    while len(springs[-1])<=groups[-1]:
         del springs[-1]
+        if len(springs[-1])==groups[-1]:
+            del groups[-1]
 
-    if len(springs[0]) < (int(groups[0]) + 1 + int(groups[1])):
-        count_arrangements(springs[0], groups[0])
-        count_arrangements(springs[1:], groups[1:])
-    elif len(springs[-1]) < (int(groups[-1]) + 1 + int(groups[-2])):
-        count_arrangements(springs[-1], groups[-1])
-        count_arrangements(springs[:-2], groups[:-2])
-
-    x = 1
+    ans1 += count_arrangements(springs, groups)
 
 
 
 
 
 ans1 = 0
-
-# Split into blocks at '.'
-
-# For every block, find maximum number of groups that fit into it (e.g. size 5 can fit 1,1,1 as #.#.#)
-
-# From the left and right, see if blocks can be thrown away (e.g. for size 2 block when groups start with 5)
 
 # Special cases:
 # n spaces for group of m: n - m + 1 options
