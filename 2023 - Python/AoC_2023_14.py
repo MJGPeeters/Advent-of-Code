64cdfc2c979c2b126f808537d185d9545e1d7e83
MJGPeeters <mjgpeeters@gmail.com>
2025-01-18 23:48:02 +0100

    2023 - Day 14 - Attempt at part II

--- 2023 - Python/AoC_2023_14.py
+++ 2023 - Python/AoC_2023_14.py
@@ -34,7 +34,6 @@ for row in platform:
 
     ans1 += load
 
-
 endTime1 = timer()
 
 print(int(ans1))
@@ -42,11 +41,20 @@ print('Time elapsed: {:.6f} s'.format(endTime1 - startTime1))
 
 # # Part II
 
-# startTime2 = timer()
+startTime2 = timer()
 
-# ans2 = 0
+ans2 = 0
 
+def tilt_platform(platform, direction):
+    """
+    Determine location of 'O' after tilting platform in a certain direction
 
+    Inputs:
+    platform - np.array with '.' as empty space, '#' as stationary stones, 'O' as rolling stones
+    direction - Integer: 0, 1, 2, 3 corresponds to north, east, south, west
+    """
 
-# print(ans2)
-# print('Time elapsed: {:.6f} s'.format(endTime2 - startTime2))
+    
+
+print(ans2)
+print('Time elapsed: {:.6f} s'.format(endTime2 - startTime2))
