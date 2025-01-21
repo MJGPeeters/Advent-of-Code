8fa5593a9f0f792c19d1dbd8c4cb726af9023c8c
MJGPeeters <mjgpeeters@gmail.com>
2025-01-21 21:58:39 +0100

    2023 - Day 14 - Solved part II
    Quite slow, ~3.7 s

--- 2023 - Python/AoC_2023_14.py
+++ 2023 - Python/AoC_2023_14.py
@@ -1,12 +1,32 @@
 from timeit import default_timer as timer
 import numpy as np
 
+def load_calculation(platform):
+    """
+    Calculate load on north support beams
+    """
+
+    total_load = 0
+
+    for col in platform.T:
+        load = 0
+        round_stones = 0
+        max_index = len(col)
+
+        for i, thing in enumerate(col):
+            if thing=='O':
+                load += max_index - i
+
+        total_load += load
+    
+    return total_load
+
 startTime1 = timer()
 
 TEST_NAME = 'Tests/Test_2023_14.txt'
 INPUT_NAME = 'Inputs/Input_2023_14.txt'
 
-with open(TEST_NAME, "r") as file:
+with open(INPUT_NAME, "r") as file:
     file_lines = file.read().splitlines()
 
 ans1 = 0
@@ -34,17 +54,15 @@ for col in platform.T:
     load += round_stones * (start_index - (round_stones - 1) / 2)
 
     ans1 += load
-
+    
 endTime1 = timer()
 
 print(int(ans1))
 print(f'Time elapsed: {endTime1 - startTime1:.6f} s')
 
-# # Part II
+# Part II
 
 startTime2 = timer()
-
-ans2 = 0
 
 def tilt_cycle_platform(platform):
     """
@@ -54,7 +72,7 @@ def tilt_cycle_platform(platform):
     platform - List of lists with '.' empty space, '#' stationary stones, 'O' rolling stones
 
     Notes:
-    On first go stones roll to the right, make sure platform is input correctly
+    Make sure platform rotation in input  is correct
     """
 
     num_tilts = 0
@@ -77,8 +95,6 @@ def tilt_cycle_platform(platform):
                     platform[start_index + round_stones:i, col_num] = '.'
                     start_index = i + 1
                     round_stones = 0
-                # elif thing=='.':
-                #     continue
 
             platform[start_index:start_index + round_stones, col_num] = 'O'
             platform[start_index + round_stones:len(col), col_num] = '.'
@@ -86,10 +102,10 @@ def tilt_cycle_platform(platform):
 
     return platform
 
-# Rotate back once, so that it goes into the module right the first time
-rock_platform = np.rot90(platform, k=1)
+rock_platform = platform
 
-num_cycles = 10000
+num_cycles = 130
+load_list = []
 
 for i in range(num_cycles):
     rock_platform_new = np.array(rock_platform)
@@ -98,12 +114,30 @@ for i in range(num_cycles):
     rock_platform_new = tilt_cycle_platform(rock_platform_new)
     rock_platform_new = np.rot90(rock_platform_new, k=1, axes=(1, 0))
 
-    if (rock_platform_new==rock_platform).all():
-        print(i)
-        break
     rock_platform = np.array(rock_platform_new)
 
-# rock_platform = np.rot90(rock_platform, k=1, axes=(1, 0))
+    load_list.append(load_calculation(rock_platform))
+
+
+check_length = 10
+break_flag = 0
+
+for start_idx in range(num_cycles):
+    test_pattern = load_list[start_idx:start_idx+check_length]
+
+    for idx in range(start_idx + 1, num_cycles):
+        if load_list[idx]==test_pattern[0] and load_list[idx:idx+check_length]==test_pattern:
+            loop_starts = [start_idx, idx]
+            break_flag = 1
+            break
+    if break_flag:
+        break
+
+cycle_length = loop_starts[1] - loop_starts[0]
+offset = loop_starts[0] % cycle_length
+cycle_index = (10**9 - offset) % cycle_length + loop_starts[0] - 1
+
+ans2 = load_list[cycle_index]
 
 endTime2 = timer()
 
