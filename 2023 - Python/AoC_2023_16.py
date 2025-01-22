c6d40aa06d8e410708f7d52d15fc8298b9e66c01
MJGPeeters <mjgpeeters@gmail.com>
2025-01-22 16:38:18 +0100

    2023 - Day 16 - Attempts at part I
    Works for test input, not for real input

--- 2023 - Python/AoC_2023_16.py
+++ 2023 - Python/AoC_2023_16.py
@@ -2,67 +2,76 @@ from timeit import default_timer as timer
 
 startTime1 = timer()
 
-testName = 'Tests/Test_2023_16.txt'
-inputName = 'Inputs/Input_2023_16.txt'
+TEST_NAME = 'Tests/Test_2023_16.txt'
+INPUT_NAME = 'Inputs/Input_2023_16.txt'
 
-with open(testName, "r") as file:
-    fileLines = file.read().splitlines()
+with open(TEST_NAME, encoding='utf-8') as file:
+    file_lines = file.read().splitlines()
 
 layout, tiles = [], []
 
-for line in fileLines:
-    layout.append([c for c in line])
-    tiles.append(['.' for c in line])
+for line in file_lines:
+    layout.append(list(line))
+    tiles.append(['.' for _ in line])
 
-def energized_tiles(layout, tiles, location, direction):
+def energized_tiles(layout, tiles, location, direction, visited):
     """
     Determine energized tiles for a light beam coming in with direction at location
     """
 
-    limit = len(tiles) - 1
+    MAX_INDEX = len(tiles) - 1
 
     row, col = location
-    tiles[row][col] = '#'
 
-    row, col = row + direction[0], col + direction[1]
-    if not (0<=row<=limit and 0<=col<=limit):
-        return tiles
-    
-    tiles[row][col] = '#'    
+    if (row, col, direction) in visited:
+        return tiles, visited
+
+    tiles[row][col] = '#'
+    visited.add((row, col, direction))
+
+    # row, col = row + direction[0], col + direction[1]
+    # if (row, col, direction) in visited or not (0<=row<=MAX_INDEX and 0<=col<=MAX_INDEX):
+    #     return tiles, visited
+
+    # tiles[row][col] = '#'
+    # visited.add((row, col, direction))
 
     while layout[row][col]=='.':
         row, col = row + direction[0], col + direction[1]
-        if not (0<=row<=limit and 0<=col<=limit):
-            return tiles
-        
+        if (row, col, direction) in visited or not (0<=row<=MAX_INDEX and 0<=col<=MAX_INDEX):
+            return tiles, visited
+
         tiles[row][col] = '#'
-    
-    if layout[row][col]=='/':
+        visited.add((row, col, direction))
+
+    tile = layout[row][col]
+
+    if tile=='/':
         directions = [(-direction[1], -direction[0])]
-    elif layout[row][col]=='\\':
+    elif tile=='\\':
         directions = [(direction[1], direction[0])]
-    elif layout[row][col]=='-' and direction[0]==0:
+    elif tile=='-' and direction[0]==0:
         directions = [direction]
-    elif layout[row][col]=='-':
+    elif tile=='-':
         directions = [(0, 1), (0, -1)]
-    elif layout[row][col]=='|' and direction[1]==0:
+    elif tile=='|' and direction[1]==0:
         directions = [direction]
-    elif layout[row][col]=='|':
+    elif tile=='|':
         directions = [(1, 0), (-1, 0)]
 
     for direction in directions:
-        tiles = energized_tiles(layout, tiles, (row, col), direction)
+        tiles, visited = energized_tiles(layout, tiles, (row, col), direction, visited)
 
-    return tiles
+    return tiles, visited
 
-ans1 = 0
+tiles, _ = energized_tiles(layout, tiles, (4, 6), (1, 0), set())
 
-tiles = energized_tiles(layout, tiles, (0, 0), (0, 1))
+ans1 = sum((row.count('#') for row in tiles))
 
 endTime1 = timer()
 
 print(ans1)
-print('Time elapsed: {:.6f} s'.format(endTime1 - startTime1))
+print(f'Time elapsed: {endTime1 - startTime1:.6f} s')
 
 # # Part II
 
