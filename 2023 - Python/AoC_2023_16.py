54fd83821d802d0fb8d93b256522de0dc8ffaf79
MJGPeeters <mjgpeeters@gmail.com>
2025-01-24 22:24:15 +0100

    2023 - Day 16 - Solved part II
    Runtime for part II is ~2 s

--- 2023 - Python/AoC_2023_16.py
+++ 2023 - Python/AoC_2023_16.py
@@ -1,47 +1,27 @@
 from timeit import default_timer as timer
 
-startTime1 = timer()
-
-TEST_NAME = 'Tests/Test_2023_16.txt'
-INPUT_NAME = 'Inputs/Input_2023_16.txt'
-
-with open(INPUT_NAME, encoding='utf-8') as file:
-    file_lines = file.read().splitlines()
-
-layout, tiles = [], []
-
-for line in file_lines:
-    layout.append(list(line))
-    tiles.append(['.' for _ in line])
-
-def energized_tiles(layout, tiles, location, direction):
+def energized_tiles(layout, tiles_set, location, direction):
     """
     Determine energized tiles for a light beam coming in with direction at location
     """
 
-    MAX_INDEX = len(tiles) - 1
+    MAX_INDEX = len(layout) - 1
 
     row, col = location
 
-    # if (row, col, direction) in visited:
-    #     return tiles
-
-    # tiles[row][col] = '#'
-    # visited.add((row, col, direction))
-
     row, col = row + direction[0], col + direction[1]
     if (row, col, direction) in visited or not (0<=row<=MAX_INDEX and 0<=col<=MAX_INDEX):
-        return tiles
+        return tiles_set
 
-    tiles[row][col] = '#'
+    tiles_set.add((row, col))
     visited.add((row, col, direction))
 
     while layout[row][col]=='.':
         row, col = row + direction[0], col + direction[1]
         if (row, col, direction) in visited or not (0<=row<=MAX_INDEX and 0<=col<=MAX_INDEX):
-            return tiles
+            return tiles_set
 
-        tiles[row][col] = '#'
+        tiles_set.add((row, col))
         visited.add((row, col, direction))
 
     tile = layout[row][col]
@@ -60,33 +40,74 @@ def energized_tiles(layout, tiles, location, direction):
         directions = [(1, 0), (-1, 0)]
 
     for direction in directions:
-        tiles = energized_tiles(layout, tiles, (row, col), direction)
+        tiles_set = energized_tiles(layout, tiles_set, (row, col), direction)
 
-    return tiles
+    return tiles_set
+
+startTime1 = timer()
+
+TEST_NAME = 'Tests/Test_2023_16.txt'
+INPUT_NAME = 'Inputs/Input_2023_16.txt'
+
+with open(INPUT_NAME, encoding='utf-8') as file:
+    file_lines = file.read().splitlines()
+
+layout = [[e for e in line] for line in file_lines]
 
 initial_location = (0, -1)
 initial_direction = (0, 1)
-
-# tiles[initial_location[0]][initial_location[1]] = '#'
 visited = set((initial_location[0], initial_location[1], initial_direction))
-tiles = energized_tiles(layout, tiles, initial_location, initial_direction)
 
-ans1 = sum((row.count('#') for row in tiles))
+tiles = energized_tiles(layout, set(), initial_location, initial_direction)
+
+ans1 = len(tiles)
 
 endTime1 = timer()
 
 print(ans1)
 print(f'Time elapsed: {endTime1 - startTime1:.6f} s')
 
-# # Part II
+# Part II
 
-# startTime2 = timer()
+startTime2 = timer()
 
-# ans2 = 0
+MAP_SIZE = len(layout)
+most_tiles = 0
 
+for i in range(MAP_SIZE):
+    # Up
+    r_start, c_start = MAP_SIZE, i
+    dir_start = (-1, 0)
+    visited = {(r_start, c_start, dir_start)}
+    tiles = energized_tiles(layout, set(), (r_start, c_start), dir_start)
+    num_tiles_up = len(tiles)
 
+    # Down
+    r_start, c_start = -1, i
+    dir_start = (1, 0)
+    visited = {(r_start, c_start, dir_start)}
+    tiles = energized_tiles(layout, set(), (r_start, c_start), dir_start)
+    num_tiles_down = len(tiles)
 
-# endTime2 = timer()
+    # Left
+    r_start, c_start = i, MAP_SIZE
+    dir_start = (0, -1)
+    visited = {(r_start, c_start, dir_start)}
+    tiles = energized_tiles(layout, set(), (r_start, c_start), dir_start)
+    num_tiles_left = len(tiles)
 
-# print(ans2)
-# print('Time elapsed: {:.6f} s'.format(endTime2 - startTime2))
+    # Right
+    r_start, c_start = i, -1
+    dir_start = (0, 1)
+    visited = {(r_start, c_start, dir_start)}
+    tiles = energized_tiles(layout, set(), (r_start, c_start), dir_start)
+    num_tiles_right = len(tiles)
+
+    tmp_most_tiles = max(num_tiles_up, num_tiles_down, num_tiles_left, num_tiles_right)
+
+    most_tiles = tmp_most_tiles if tmp_most_tiles>most_tiles else most_tiles
+
+endTime2 = timer()
+
+print(most_tiles)
+print('Time elapsed: {:.6f} s'.format(endTime2 - startTime2))
