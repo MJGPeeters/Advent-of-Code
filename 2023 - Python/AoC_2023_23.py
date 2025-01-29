016d76a021cc8b5b7ab7735d65c4a6d95e36ad9d
MJGPeeters <mjgpeeters@gmail.com>
2025-01-29 15:17:03 +0100

    2023 - Day 23 - Minor changes

--- 2023 - Python/AoC_2023_23.py
+++ 2023 - Python/AoC_2023_23.py
@@ -1,15 +1,14 @@
 from timeit import default_timer as timer
 import copy
 
-def find_all_paths(start_loc, goal_loc, map_dict):
+def find_all_paths(pos, goal_loc, map_dict):
     """
     Find the lengths of all paths from start to goal recursively
     """
 
-    diffs = (1, complex(0,1), -1, complex(0,-1))
+    diffs = (1, 1j, -1, -1j)
     out_dict = {1: 'v', -1: '^', 1j: '>', -1j: '<'}
 
-    pos = start_loc
     map_dict[pos] = '#'
 
     path_length = 1
@@ -27,16 +26,15 @@ def find_all_paths(start_loc, goal_loc, map_dict):
             map_dict[pos] = '#'
             path_length += 1
         else:
-            for pos, _, _ in neighbors:
+            for neighbor_pos, _, _ in neighbors:
                 new_map_dict = copy.copy(map_dict)
-                tmp = find_all_paths(pos, goal_loc, new_map_dict)
+                tmp = find_all_paths(neighbor_pos, goal_loc, new_map_dict)
                 if isinstance(tmp, int):
                     next_path_lengths.append(tmp)
                 else:
                     next_path_lengths += tmp
 
-            out = [path_length + next_length for next_length in next_path_lengths]
-            return out
+            return [path_length + next_length for next_length in next_path_lengths]
 
 start_time_1 = timer()
 
@@ -56,45 +54,21 @@ GOAL = complex(FOREST_SIZE - 1, FOREST_SIZE - 2)
 paths = find_all_paths(START, GOAL, forest_map)
 
 ans1 = max(paths) + 1
-# First go through the map and find all the crossroads. For every crossroads, check how many
-# roads go into and out of the crossroads. Only consider the crossroads with multiple roads
-# going out.
-
-
-# for i_row, row in enumerate(FOREST_MAP):
-#     for i_col, col in enumerate(row):
-#         if col=='.' and FOREST_MAP[i_row][i_col]!='.'
-
-# crossroads = [(r,c) for r in FOREST_MAP for c in r if c=='.' and ]
-
-# For all those crossroads, find to what other crossroads they connect, and what the distance
-# between them is.
-
-# Make a graph of this, stored in a dictionary:
-#       {crossroads coordinates: (distance, (next crossroads coordinates))}
-#       {(2, 3): (10, (5, 8)), (15, (8, 12))}
-
-# Then start at the initial position, find the first crossroads (and distance to it). From there,
-# every time you encounter a crossroads, check both paths by looking up in the dictionary where
-# they lead. Do this recursively, until all paths end at the goal coordinates (or reach a dead end).
-
-
-# Check which path has the longest length.
 
 end_time_1 = timer()
 
 print(ans1)
 print(f'Time elapsed: {end_time_1 - start_time_1:.6f} s')
 
-# # Part II
+# Part II
 
-# start_time_2 = timer()
+start_time_2 = timer()
 
-# ans2 = 0
+ans2 = 0
 
+# Change possible 
 
+end_time_2 = timer()
 
-# end_time_2 = timer()
-
-# print(ans2)
-# print(f'Time elapsed: {end_time_2 - start_time_2:.6f} s')
+print(ans2)
+print(f'Time elapsed: {end_time_2 - start_time_2:.6f} s')
