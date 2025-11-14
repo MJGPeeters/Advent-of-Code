1a2948ed722808602bb925b1e97749f7c0fc8173
MJGPeeters <mjgpeeters@gmail.com>
2025-11-14 14:14:59 +0100

    2022 - Day 1 - Solved Part 1

---
+++ 2022 - Python/Day1/Day1.py
@@ -0,0 +1,31 @@
+from timeit import default_timer as timer
+
+
+start_time_1 = timer()
+
+TEST = False
+
+if TEST:
+    FILE_NAME = "Example.txt"
+else:
+    FILE_NAME = "Input.txt"
+
+with open(FILE_NAME, 'r') as file:
+    data = [line.strip() for line in file]
+
+max_calories = 0
+current_calories = 0
+
+for calories in data:
+    if calories is not '':
+        current_calories += int(calories)
+    elif current_calories > max_calories:
+        max_calories = current_calories
+        current_calories = 0
+    else:
+        current_calories = 0
+
+end_time_1 = timer()
+
+print(max_calories)
+print(f'Time elapsed: {end_time_1 - start_time_1:.6f} s')
