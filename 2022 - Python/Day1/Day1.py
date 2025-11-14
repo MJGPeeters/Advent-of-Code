3c42037a86bae8dc740396c1e0c40267badb0a91
MJGPeeters <mjgpeeters@gmail.com>
2025-11-14 14:34:50 +0100

    2022 - Day 1 - Solved Part 2

--- 2022 - Python/Day1/Day1.py
+++ 2022 - Python/Day1/Day1.py
@@ -1,6 +1,5 @@
 from timeit import default_timer as timer
 
-
 start_time_1 = timer()
 
 TEST = False
@@ -13,19 +12,31 @@ else:
 with open(FILE_NAME, 'r') as file:
     data = [line.strip() for line in file]
 
-max_calories = 0
+data.append('')
+
+max_calories = [0,0,0]
 current_calories = 0
 
 for calories in data:
     if calories is not '':
         current_calories += int(calories)
-    elif current_calories > max_calories:
-        max_calories = current_calories
+    elif current_calories > max_calories[0]:
+        max_calories[2] = max_calories[1]
+        max_calories[1] = max_calories[0]
+        max_calories[0] = current_calories
+        current_calories = 0
+    elif current_calories > max_calories[1]:
+        max_calories[2] = max_calories[1]
+        max_calories[1] = current_calories
+        current_calories = 0
+    elif current_calories > max_calories[2]:
+        max_calories[2] = current_calories
         current_calories = 0
     else:
         current_calories = 0
 
 end_time_1 = timer()
 
-print(max_calories)
+print(max_calories[0])
+print(sum(max_calories))
 print(f'Time elapsed: {end_time_1 - start_time_1:.6f} s')
