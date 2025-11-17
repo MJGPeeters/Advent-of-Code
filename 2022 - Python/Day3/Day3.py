2bd540687725b41929ed7896a9341b9412bd4a36
MJGPeeters <mjgpeeters@gmail.com>
2025-11-17 16:04:16 +0100

    2022 - Day 3 - Solved Parts 1 & 2

---
+++ 2022 - Python/Day3/Day3.py
@@ -0,0 +1,45 @@
+from timeit import default_timer as timer
+
+start_time_1 = timer()
+
+TEST = False
+
+if TEST:
+    FILE_NAME = "Example.txt"
+else:
+    FILE_NAME = "Input.txt"
+
+with open(FILE_NAME, 'r') as file:
+    rucksacks = [line.split() for line in file]
+
+sum_priorities = 0
+
+for rucksack in rucksacks:
+    contents = rucksack[0]
+    compartment_size = int(len(contents)/2)
+    for element in contents[0:compartment_size]:
+        if element in contents[compartment_size:]:
+            if element.isupper():
+                sum_priorities += ord(element) - 38
+            else:
+                sum_priorities += ord(element) - 96
+            break
+
+sum_priorities_badges = 0
+
+for group in range(0,int(len(rucksacks)/3)):
+    rucksack_group = [rucksacks[3*group], rucksacks[3*group+1], rucksacks[3*group+2]]
+    for element in rucksack_group[0][0]:
+        if element in rucksack_group[1][0]:
+            if element in rucksack_group[2][0]:
+                if element.isupper():
+                    sum_priorities_badges += ord(element) - 38
+                else:
+                    sum_priorities_badges += ord(element) - 96
+                break   
+
+end_time_1 = timer()
+
+print(sum_priorities)
+print(sum_priorities_badges)
+print(f'Time elapsed: {end_time_1 - start_time_1:.6f} s')
