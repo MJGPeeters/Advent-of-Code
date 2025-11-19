4e0ae5f247600265fce95006d676e9b72dfa4af6
MJGPeeters <mjgpeeters@gmail.com>
2025-11-19 12:25:36 +0100

    2022 - Day 7 - First attempt at Part 1

--- 2022 - Python/Day7/Day7.py
+++ 2022 - Python/Day7/Day7.py
@@ -10,10 +10,34 @@ if TEST:
 else:
     FILE_NAME = "Day" + str(DAY_NUMBER) + "/Input.txt"
 
+# def make_file_system(input_file_system, contents):
+
+
+#     return output_file_system
+
 with open(FILE_NAME, 'r') as file:
     terminal_output = [line.strip() for line in file]
 
+FILE_START = '0123456789'
+DIR_START = 'd'
+file_system = {}
 size_sum = 0
+
+## Go through terminal output and find relevant groups of output
+for line in terminal_output:
+    if line=='$ cd /' or line=='$ ls':
+        continue
+
+
+
+    if line[0] in FILE_START:
+        file_system
+
+    if line[0] in DIR_START:
+        file_system
+
+    if line=='$ cd'
+
 
 end_time_1 = timer()
 
