c07b50ff7d3acfe55ca09ca97892cfa5bc96e928
MJGPeeters <mjgpeeters@gmail.com>
2025-11-19 15:16:46 +0100

    2022 - Day 8 - Rewrote Part 1
    Rewrote Part 1 to use the same approach as Part 2, to decrease runtime

--- 2022 - Python/Day8/Day8.py
+++ 2022 - Python/Day8/Day8.py
@@ -10,50 +10,17 @@ if TEST:
 else:
     FILE_NAME = "Day" + str(DAY_NUMBER) + "/Input.txt"
 
-def is_tree_visible(tree_map,row,col):
+def check_tree_view(tree_map,row,col):
     map_size = len(tree_map)
-    visible_from_left, visible_from_right = 1, 1
-    visible_from_top, visible_from_bottom = 1, 1
-    tree_size = tree_map[row][col]
 
-    # Check if visible from left
-    for i in range(col):
-        tmp = tree_map[row][i]
-        if tree_size<=tree_map[row][i]:
-            visible_from_left = 0
-            break
-    if visible_from_left:
-        return 1
+    distance_left = col
+    distance_right = map_size-col-1
+    distance_top = row
+    distance_bottom = map_size-row-1
 
-    # Check if visible from right
-    for i in range(map_size-1,col,-1):
-        if tree_size<=tree_map[row][i]:
-            visible_from_right = 0
-            break
-    if visible_from_right:
-        return 1
-
-    # Check if visible from top
-    for i in range(row):
-        if tree_size<=tree_map[i][col]:
-            visible_from_top = 0
-            break
-    if visible_from_top:
-        return 1
-
-    # Check if visible from bottom
-    for i in range(map_size-1,row,-1):
-        if tree_size<=tree_map[i][col]:
-            visible_from_bottom = 0
-            break
-    if visible_from_bottom:
-        return 1
-    
-    return 0
-
-def find_scenic_score(tree_map,row,col):
     visible_trees_left, visible_trees_right = 0, 0
     visible_trees_top, visible_trees_bottom = 0, 0
+
     tree_size = tree_map[row][col]
 
     # Check to the left
@@ -79,8 +46,18 @@ def find_scenic_score(tree_map,row,col):
         visible_trees_bottom += 1
         if tree_map[i][col]>=tree_size:
             break
-    
-    return visible_trees_left*visible_trees_right*visible_trees_top*visible_trees_bottom
+
+    if (visible_trees_left==distance_left       and tree_map[row][0]<tree_size) or \
+       (visible_trees_right==distance_right     and tree_map[row][map_size-1]<tree_size) or \
+       (visible_trees_top==distance_top         and tree_map[0][col]<tree_size) or \
+       (visible_trees_bottom==distance_bottom   and tree_map[map_size-1][col]<tree_size):
+        visible_bool = 1
+    else:
+        visible_bool = 0
+
+    scenic_score = visible_trees_left*visible_trees_right*visible_trees_top*visible_trees_bottom
+
+    return visible_bool, scenic_score
 
 with open(FILE_NAME, 'r') as file:
     tree_map = [line.strip() for line in file]
@@ -91,11 +68,8 @@ max_scenic_score = 0
 
 for i in range(1,map_size-1):
     for j in range(1,map_size-1):
-        visible_trees += is_tree_visible(tree_map,i,j)
-
-for i in range(1,map_size-1):
-    for j in range(1,map_size-1):
-        scenic_score = find_scenic_score(tree_map,i,j)
+        visible_bool, scenic_score = check_tree_view(tree_map,i,j)
+        visible_trees += visible_bool
         if scenic_score>max_scenic_score:
             max_scenic_score = scenic_score
 
