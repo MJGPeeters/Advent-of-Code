50189676b58f188a63b609e16322a92762250476
MJGPeeters <mjgpeeters@gmail.com>
2025-11-25 17:19:36 +0100

    2022 - Day 12 - Solved Part 2

--- 2022 - Python/Day12/Day12.py
+++ 2022 - Python/Day12/Day12.py
@@ -2,41 +2,69 @@ from timeit import default_timer as timer
 import math as m
 import cmath as cm
 
-def dijkstra_height(height_map, start, end):
+# For Part 2 it is easiest to reverse the problem, so to start the search at 'E' and then 
+# find the shortest route to an 'a'. Therefore, this method is used for both Part 1 & 2. 
 
-    dist_map = [[m.inf for _ in line] for line in height_map]
-    dist_map[start[0]][start[1]] = 0
+def dijkstra_height(input_map, goal='S'):
+    height_map = []
+    dist_map = [[m.inf for _ in line] for line in input_map]
+    
+    for row_idx, row in enumerate(input_map):
+        if 'S' in row and 'E' in row:
+            col_idx_s = row.find('S')
+            S_pos = (row_idx, col_idx_s)
+            col_idx_e = row.find('E')
+            dist_map[row_idx][col_idx_e] = 0
+            init_pos = (row_idx, col_idx_e)
+            height_map.append('a' + input_map[row_idx][1:col_idx_e] + 'z' + input_map[row_idx][col_idx_e+1:])
+
+        elif 'S' in row:
+            col_idx_s = row.find('S')
+            S_pos = (row_idx, col_idx_s)
+            height_map.append(input_map[row_idx][0:col_idx_s] + 'a' + input_map[row_idx][col_idx_s+1:])
+
+        elif 'E' in row:
+            col_idx_e = row.find('E')
+            dist_map[row_idx][col_idx_e] = 0
+            init_pos = (row_idx, col_idx_e)
+            height_map.append(input_map[row_idx][0:col_idx_e] + 'z' + input_map[row_idx][col_idx_e+1:])
+        
+        else:
+            height_map.append(input_map[row_idx])
 
     min_row, max_row = 0, len(height_map)-1
     min_col, max_col = 0, len(height_map[0])-1
 
-    open_set = [start]
+    open_set = [init_pos]
 
     while open_set:
-        current_square = open_set.pop(0)
-        current_dist = dist_map[current_square[0]][current_square[1]]
+        cur_sqr = open_set.pop(0)
+        cur_dst = dist_map[cur_sqr[0]][cur_sqr[1]]
 
-        if current_square==end:
-            return dist_map[current_square[0]][current_square[1]]
+        if goal=='S':
+            if cur_sqr==S_pos:
+                return dist_map[cur_sqr[0]][cur_sqr[1]]
+        else:
+            if height_map[cur_sqr[0]][cur_sqr[1]]==goal:
+                return dist_map[cur_sqr[0]][cur_sqr[1]]
+                
+        ngbs = [(cur_sqr[0] + 0, cur_sqr[1] + 1),
+                     (cur_sqr[0] + 0, cur_sqr[1] - 1),
+                     (cur_sqr[0] + 1, cur_sqr[1] + 0),
+                     (cur_sqr[0] - 1, cur_sqr[1] + 0)]
 
-        neighbors = [(current_square[0] + 0, current_square[1] + 1),
-                     (current_square[0] + 0, current_square[1] - 1),
-                     (current_square[0] + 1, current_square[1] + 0),
-                     (current_square[0] - 1, current_square[1] + 0)]
-
-        for neighbor in neighbors:
+        for ngb in ngbs:
             # Check if neighbor is not out of bounds
-            if (neighbor[0] < min_row or neighbor[0] > max_row) or (neighbor[1] < min_col or neighbor[1] > max_col):
+            if (ngb[0] < min_row or ngb[0] > max_row) or (ngb[1] < min_col or ngb[1] > max_col):
                 continue
             
             # Check if elevation difference to neighbor is not too big
-            if ord(height_map[neighbor[0]][neighbor[1]]) - ord(height_map[current_square[0]][current_square[1]]) > 1:
+            if ord(height_map[ngb[0]][ngb[1]]) - ord(height_map[cur_sqr[0]][cur_sqr[1]]) < -1:
                 continue
 
-            if current_dist + 1 < dist_map[neighbor[0]][neighbor[1]]:
-                dist_map[neighbor[0]][neighbor[1]] = current_dist + 1
-                open_set.append(neighbor)
-
+            if cur_dst + 1 < dist_map[ngb[0]][ngb[1]]:
+                dist_map[ngb[0]][ngb[1]] = cur_dst + 1
+                open_set.append(ngb)
 
 start_time_1 = timer()
 
@@ -51,19 +79,11 @@ else:
 with open(FILE_NAME, 'r') as file:
     heightmap = [line.strip() for line in file]
 
-for row_idx, row in enumerate(heightmap):
-    if 'S' in row:
-        col_idx = row.find('S')
-        start_pos = (row_idx, col_idx)
-        heightmap[row_idx] = heightmap[row_idx][0:col_idx] + 'a' + heightmap[row_idx][col_idx+1:]
-    if 'E' in row:
-        col_idx = row.find('E')
-        end_pos = (row_idx, col_idx)
-        heightmap[row_idx] = heightmap[row_idx][0:col_idx] + 'z' + heightmap[row_idx][col_idx+1:]
-
-num_steps = dijkstra_height(heightmap, start_pos, end_pos)
+num_steps_S = dijkstra_height(heightmap)
+num_steps_a = dijkstra_height(heightmap, 'a')
 
 end_time_1 = timer()
 
-print(num_steps)
+print(num_steps_S)
+print(num_steps_a)
 print(f'Time elapsed: {end_time_1 - start_time_1:.6f} s')
