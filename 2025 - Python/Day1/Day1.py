fbae80364d401b3b2f11979ed35c755422bf21fd
MJGPeeters <mjgpeeters@gmail.com>
2025-12-01 11:02:23 +0100

    2025 - Day 1 - Solved Part 2

--- 2025 - Python/Day1/Day1.py
+++ 2025 - Python/Day1/Day1.py
@@ -13,7 +13,7 @@ else:
 with open(FILE_NAME, 'r') as file:
     rotations = [line.strip() for line in file]
 
-dial_value, password = 50, 0
+dial_value, password_old, password_new = 50, 0, 0
 
 for rotation in rotations:
     if rotation[0]=='L':
@@ -22,9 +22,29 @@ for rotation in rotations:
         dial_value = (dial_value + int(rotation[1:]))%100
     
     if dial_value==0:
-        password += 1
+        password_old += 1
+
+dial_value = 50
+
+for rotation in rotations:
+    if rotation[0]=='L':
+        new_dial_value = dial_value - int(rotation[1:])
+        if dial_value==0:
+            password_new += abs(new_dial_value+1)//100
+        elif new_dial_value<0:
+            password_new += 1 + abs(new_dial_value+1)//100
+    else:
+        new_dial_value = dial_value + int(rotation[1:])
+        if new_dial_value>99:
+            password_new += abs(new_dial_value-1)//100
+    
+    dial_value = new_dial_value%100
+
+    if dial_value==0:
+        password_new += 1
 
 end_time = timer()
 
-print(password)
+print(password_old)
+print(password_new)
 print(f'Time elapsed: {end_time - start_time:.6f} s')
