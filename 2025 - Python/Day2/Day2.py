51e0bd710dfc5274f176e28fa35c32e7ef32a373
MJGPeeters <mjgpeeters@gmail.com>
2025-12-02 17:58:37 +0100

    2025 - Day 2 - Refactored Parts 1&2
    Decreased number of IDs that need to be checked, running time now ~2 ms

--- 2025 - Python/Day2/Day2.py
+++ 2025 - Python/Day2/Day2.py
@@ -1,4 +1,5 @@
 from timeit import default_timer as timer
+import math as m
 
 start_time = timer()
 
@@ -14,27 +15,39 @@ for line in open(FILE_NAME, 'r'):
     ID_ranges = line.strip().split(',')
 
 prime_factors = {2:(1,), 3:(1,), 4:(1,2), 5:(1,), 6:(1,2,3), 7:(1,), 8:(1,2,4), 9:(1,3), 10:(1,2,5)}
-invalid_ID_accum_1, invalid_ID_accum_2 = 0, 0
+
+invalid_ID_accum_p1, invalid_ID_accum_p2 = 0, 0
 
 for ID_range in ID_ranges:
-    start_ID, end_ID = ID_range.split('-')
+    start_ID, end_ID = map(int, ID_range.split('-'))
 
-    ID_lengths = [i for i in range(len(start_ID), len(end_ID) + 1) if i>1]
+    ID_lengths = [i for i in range(len(str(start_ID)), len(str(end_ID)) + 1) if i>1]
     invalid_IDs = set()
 
-    for ID_length in ID_lengths:
+    for length_idx, ID_length in enumerate(ID_lengths):
         for sequence_length in prime_factors[ID_length]:
-            for sub_sequence in range(10**(sequence_length-1),10**sequence_length):
+            if len(ID_lengths)==1:
+                sub_sequence_start = start_ID//10**(ID_length-sequence_length)
+                sub_sequence_end = end_ID//10**(ID_length-sequence_length)
+            elif length_idx==0:
+                sub_sequence_start = start_ID//10**(ID_length-sequence_length)
+                sub_sequence_end = 10**sequence_length - 1
+            else:
+                sub_sequence_start = 10**(sequence_length-1)
+                sub_sequence_end = end_ID//10**(ID_length-sequence_length)
+
+            for sub_sequence in range(sub_sequence_start,sub_sequence_end+1):
                 test_ID = int(str(sub_sequence)*int(ID_length/sequence_length))
-                if test_ID<=int(end_ID) and test_ID>=int(start_ID) and test_ID not in invalid_IDs:
-                    string_ID = str(test_ID)
-                    if string_ID[0:int(len(string_ID)/2)]==string_ID[int(len(string_ID)/2):]:
-                        invalid_ID_accum_1 += test_ID                    
+
+                if test_ID<=end_ID and test_ID>=start_ID and test_ID not in invalid_IDs:
+                    if test_ID//10**int(ID_length/2)==test_ID%10**int(ID_length/2):
+                        invalid_ID_accum_p1 += test_ID 
+
                     invalid_IDs.add(test_ID)
-                    invalid_ID_accum_2 += test_ID
+                    invalid_ID_accum_p2 += test_ID
 
 end_time = timer()
 
-print(invalid_ID_accum_1)
-print(invalid_ID_accum_2)
+print(invalid_ID_accum_p1)
+print(invalid_ID_accum_p2)
 print(f'Time elapsed: {(end_time - start_time)*1000:.3f} ms')
