7765204b513756ed05557a6c8391305c3a946780
MJGPeeters <mjgpeeters@gmail.com>
2025-12-03 10:15:22 +0100

    2025 - Day 3 - Refactored Parts 1&2
    Put the calculation in a function, with the number of batteries as input. Total running time ~6 ms

--- 2025 - Python/Day3/Day3.py
+++ 2025 - Python/Day3/Day3.py
@@ -1,5 +1,18 @@
 from timeit import default_timer as timer
 
+def max_joltage(input_banks, num_batteries):
+    total_joltage = 0
+    
+    for bank in input_banks:
+        idx = 0
+        for n in range(num_batteries-1,-1,-1):
+            max_battery = max(bank[idx:len(bank)-n])
+            idx += 1 + bank[idx:].index(max_battery)
+
+            total_joltage += max_battery * (10**n)
+
+    return total_joltage
+
 start_time = timer()
 
 DAY_NUMBER = 3
@@ -13,23 +26,8 @@ else:
 with open(FILE_NAME, 'r') as file:
     banks = [[int(x) for x in line.strip()] for line in file]
 
-total_joltage_p1, total_joltage_p2 = 0, 0
-
-for bank in banks:
-    max_tens = max(bank[0:-1])
-    idx_max_tens = bank.index(max_tens)
-
-    max_ones = max(bank[idx_max_tens+1:])
-
-    total_joltage_p1 += 10*max_tens + max_ones
-
-for bank in banks:
-    idx_max_i = -1
-    for i in range(1,13):
-        max_i = max(bank[idx_max_i+1:len(bank)-12+i])
-        idx_max_i += 1 + bank[idx_max_i+1:].index(max_i)
-
-        total_joltage_p2 += max_i * (10**(12-i))
+total_joltage_p1 = max_joltage(banks, 2)
+total_joltage_p2 = max_joltage(banks, 12)
 
 end_time = timer()
 
