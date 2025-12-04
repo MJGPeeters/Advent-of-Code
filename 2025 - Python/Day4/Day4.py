b70a0c6cb9b77802dd3f23b82fb8eaef7fb0c821
MJGPeeters <mjgpeeters@gmail.com>
2025-12-04 10:13:47 +0100

    2025 - Day 4 - Solved Part 2
    Total running time ~400 ms

--- 2025 - Python/Day4/Day4.py
+++ 2025 - Python/Day4/Day4.py
@@ -1,5 +1,4 @@
 from timeit import default_timer as timer
-import cmath
 
 start_time = timer()
 
@@ -12,16 +11,31 @@ else:
     FILE_NAME = "Day" + str(DAY_NUMBER) + "/Input.txt"
 
 with open(FILE_NAME, 'r') as file:
-    paper_map = {complex(row,col) for row, line in enumerate(file) for col, element in enumerate(line) if element=='@'}
+    paper_map = {complex(row,col):1 for row, line in enumerate(file) for col, element in enumerate(line) if element=='@'}
 
 neighbors = {1, 1+1j, 1j, -1+1j, -1, -1-1j, -1j, 1-1j}
-num_rolls = 0
+num_rolls_p1, num_rolls_p2 = 0, 0
 
 for roll in paper_map:
     if sum(neighbor+roll in paper_map for neighbor in neighbors)<4:
-        num_rolls += 1
+        num_rolls_p1 += 1
+
+while True:
+    removable_rolls = set()
+
+    for roll in paper_map:
+        if sum(neighbor+roll in paper_map for neighbor in neighbors)<4:
+            removable_rolls.add(roll)
+            num_rolls_p2 += 1
+
+    if not removable_rolls:
+        break
+
+    for roll in removable_rolls:
+        del paper_map[roll]
 
 end_time = timer()
 
-print(num_rolls)
+print(num_rolls_p1)
+print(num_rolls_p2)
 print(f'Time elapsed: {(end_time - start_time)*1000:.3f} ms')
