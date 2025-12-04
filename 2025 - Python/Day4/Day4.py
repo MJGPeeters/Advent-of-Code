4d7e8b2b8b24c960bf1e721f396f751cd2ced6ed
MJGPeeters <mjgpeeters@gmail.com>
2025-12-04 18:41:55 +0100

    2025 - Day 4 - Refactored Parts 1&2 for speed-up
    Total running time now ~80 ms

--- 2025 - Python/Day4/Day4.py
+++ 2025 - Python/Day4/Day4.py
@@ -14,25 +14,33 @@ with open(FILE_NAME, 'r') as file:
     paper_map = {complex(row,col):1 for row, line in enumerate(file) for col, element in enumerate(line) if element=='@'}
 
 neighbors = {1, 1+1j, 1j, -1+1j, -1, -1-1j, -1j, 1-1j}
-num_rolls_p1, num_rolls_p2 = 0, 0
+to_be_removed, to_be_checked = set(), set()
+num_rolls_p1 = 0
 
 for roll in paper_map:
     if sum(neighbor+roll in paper_map for neighbor in neighbors)<4:
+        to_be_removed.add(roll)
+        if roll in to_be_checked:
+            to_be_checked.remove(roll)
+        to_be_checked.update(roll+neighbor for neighbor in neighbors if (roll+neighbor in paper_map and roll+neighbor not in to_be_removed))
         num_rolls_p1 += 1
 
-while True:
-    removable_rolls = set()
+num_rolls_p2 = num_rolls_p1
 
-    for roll in paper_map:
+while to_be_checked:
+    for roll in to_be_removed:
+        del paper_map[roll]
+    to_be_removed, to_be_checked_new = set(), set()
+
+    for roll in to_be_checked:
         if sum(neighbor+roll in paper_map for neighbor in neighbors)<4:
-            removable_rolls.add(roll)
+            to_be_removed.add(roll)
+            if roll in to_be_checked_new:
+                to_be_checked_new.remove(roll)
+            to_be_checked_new.update(roll+neighbor for neighbor in neighbors if (roll+neighbor in paper_map and roll+neighbor not in to_be_removed))
             num_rolls_p2 += 1
 
-    if not removable_rolls:
-        break
-
-    for roll in removable_rolls:
-        del paper_map[roll]
+    to_be_checked = to_be_checked_new
 
 end_time = timer()
 
