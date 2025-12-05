14b07961899008f361ac190d2c260732f95a8c8f
MJGPeeters <mjgpeeters@gmail.com>
2025-12-05 09:16:29 +0100

    2025 - Day 4 - Refactored Parts 1&2
    Put relevant logic in function, total running time still ~80 ms

--- 2025 - Python/Day4/Day4.py
+++ 2025 - Python/Day4/Day4.py
@@ -1,5 +1,23 @@
 from timeit import default_timer as timer
 
+def remove_paper_rolls(to_be_removed, to_be_checked, paper_map_dict):
+    for roll in to_be_removed:
+        del paper_map[roll]
+
+    neighbors = {1, 1+1j, 1j, -1+1j, -1, -1-1j, -1j, 1-1j}
+    to_be_removed, to_be_checked_next = set(), set()
+    num_rolls = 0
+
+    for roll in to_be_checked:
+        if sum(neighbor+roll in paper_map_dict for neighbor in neighbors)<4:
+            to_be_removed.add(roll)
+            if roll in to_be_checked_next:
+                to_be_checked_next.remove(roll)
+            to_be_checked_next.update(roll+neighbor for neighbor in neighbors if (roll+neighbor in paper_map and roll+neighbor not in to_be_removed))
+            num_rolls += 1
+
+    return to_be_removed, to_be_checked_next, num_rolls
+
 start_time = timer()
 
 DAY_NUMBER = 4
@@ -13,34 +31,16 @@ else:
 with open(FILE_NAME, 'r') as file:
     paper_map = {complex(row,col):1 for row, line in enumerate(file) for col, element in enumerate(line) if element=='@'}
 
-neighbors = {1, 1+1j, 1j, -1+1j, -1, -1-1j, -1j, 1-1j}
-to_be_removed, to_be_checked = set(), set()
-num_rolls_p1 = 0
-
-for roll in paper_map:
-    if sum(neighbor+roll in paper_map for neighbor in neighbors)<4:
-        to_be_removed.add(roll)
-        if roll in to_be_checked:
-            to_be_checked.remove(roll)
-        to_be_checked.update(roll+neighbor for neighbor in neighbors if (roll+neighbor in paper_map and roll+neighbor not in to_be_removed))
-        num_rolls_p1 += 1
-
-num_rolls_p2 = num_rolls_p1
+to_be_removed, to_be_checked = set(), paper_map
+num_rolls_p1, num_rolls_p2 = 0, 0
 
 while to_be_checked:
-    for roll in to_be_removed:
-        del paper_map[roll]
-    to_be_removed, to_be_checked_new = set(), set()
+    to_be_removed, to_be_checked, num_removed_rolls = remove_paper_rolls(to_be_removed, to_be_checked, paper_map)
 
-    for roll in to_be_checked:
-        if sum(neighbor+roll in paper_map for neighbor in neighbors)<4:
-            to_be_removed.add(roll)
-            if roll in to_be_checked_new:
-                to_be_checked_new.remove(roll)
-            to_be_checked_new.update(roll+neighbor for neighbor in neighbors if (roll+neighbor in paper_map and roll+neighbor not in to_be_removed))
-            num_rolls_p2 += 1
+    if num_rolls_p1==0:
+        num_rolls_p1 = num_removed_rolls
 
-    to_be_checked = to_be_checked_new
+    num_rolls_p2 += num_removed_rolls
 
 end_time = timer()
 
