fd7631f7cf43613208dd2d7bc3cc2d2420613bd5
MJGPeeters <mjgpeeters@gmail.com>
2025-12-07 17:39:58 +0100

    2025 - Day 7 - Refactored Parts 1 & 2
    Total running time ~3 ms

--- 2025 - Python/Day7/Day7.py
+++ 2025 - Python/Day7/Day7.py
@@ -1,46 +1,28 @@
 from timeit import default_timer as timer
 
-def num_timelines(splitter_set, diagram_height, splitter, mem_dict):
-    if splitter in mem_dict:
-        return mem_dict[splitter]
-    else:
-        timelines = 0
-    
-    # Determine new paths (splitter-1, splitter+1)
-    splitter_row, splitter_col = splitter
-    left_col = splitter_col - 1
-    right_col = splitter_col + 1
+def num_timelines(splitter, splitter_dict):
+    if splitter in splitter_dict:
+        return splitter_dict[splitter], splitter_dict
 
-    # Find next splitter on each path
-    left_splitter, right_splitter = (), ()
-    
-    for i in range(splitter_row+2, diagram_height, 2):
-        if (i,left_col) in splitter_set:
-            left_splitter = (i,left_col)
-            break
+    timelines = 0
 
-    for i in range(splitter_row+2, diagram_height, 2):
-        if (i,right_col) in splitter_set:
-            right_splitter = (i,right_col)
-            break        
+    for side in (-1, 1):
+        new_splitter = ()
+        new_col = splitter[1] + side
 
-    # If there is no next splitter, this counts as one extra timeline,
-    # if there is a next splitter, add num_timelines(splitter_set, NEW_splitter)
-    if not left_splitter:
-        timelines += 1
-    else:
-        timelines += num_timelines(splitter_set, diagram_height, left_splitter, mem_dict)
+        for i in range(splitter[0]+2, diagram_height, 2):
+            if (i,new_col) in splitters:
+                new_splitter = (i,new_col)
+                break
 
-    if not right_splitter:
-        timelines += 1
-    else:
-        timelines += num_timelines(splitter_set, diagram_height, right_splitter, mem_dict)
+        if not new_splitter:
+            timelines += 1
+        else:
+            timelines += num_timelines(new_splitter, splitter_dict)[0]
 
-    mem_dict[splitter] = timelines
+    splitter_dict[splitter] = timelines
 
-    return timelines
-
-    # Use memoization to store the total amount of timelines after a certain splitter once it's known
+    return timelines, splitter_dict
 
 start_time = timer()
 
@@ -52,29 +34,12 @@ if TEST:
 else:
     FILE_NAME = "Day" + str(DAY_NUMBER) + "/Input.txt"
 
-# split_count = 0
-
-# for line_number, line in enumerate(open(FILE_NAME, 'r')):
-#     if line_number==0:
-#         tachyons = {line.find('S')}
-#         continue
-#     elif line_number%2==1:
-#         continue
-
-#     splitters = (idx for idx, x in enumerate(line) if x=='^')
-
-#     for splitter in splitters:
-#         if splitter in tachyons:
-#             tachyons.remove(splitter)
-#             tachyons.add(splitter-1)
-#             tachyons.add(splitter+1)
-#             split_count += 1
-
 with open(FILE_NAME, 'r') as file:
     manifold_diagram = [line.strip() for line in file]
 
+diagram_height = len(manifold_diagram)
 splitters = set()
-mem_dict = {}
+splitter_dict = {}
 
 for row_num, row in enumerate(manifold_diagram):
     if row_num==0:
@@ -82,9 +47,11 @@ for row_num, row in enumerate(manifold_diagram):
     elif row_num%2==0:
         splitters.update((row_num,col_num) for col_num, x in enumerate(row) if x=='^')
 
-timelines = num_timelines(splitters, len(manifold_diagram), (2,initial_col), {})
+total_timelines, splitter_dict = num_timelines((2,initial_col), {})
+total_splits = len(splitter_dict)
 
 end_time = timer()
 
-print(timelines)
+print(total_splits)
+print(total_timelines)
 print(f'Time elapsed: {(end_time - start_time)*1000:.3f} ms')
